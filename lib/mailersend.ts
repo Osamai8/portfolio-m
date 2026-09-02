1ba3d67090fac61d5d72607760b2812a434d9ad2
Osama Islam <islamosama488@gmail.com>
2026-09-02 23:57:13 +0530

    update

--- lib/mailersend.ts
+++ lib/mailersend.ts
@@ -1,6 +1,6 @@
 import { MailerSend } from "mailersend";
 
 export const mailerSend = new MailerSend({
-    apiKey: process.env.MAILERSEND_API_KEY!,
+    apiKey: process.env.MAILERSEND_CHOCOCHIP!,
 
 });
