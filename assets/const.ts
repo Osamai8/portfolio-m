5a39947e175182bb8fb941f6a91036a53c1023e2
Osama Islam <islamosama488@gmail.com>
2026-09-03 13:31:50 +0530

    resume link added

--- assets/const.ts
+++ assets/const.ts
@@ -2,7 +2,7 @@ export const LINKS = {
     instagram: "https://www.instagram.com/_osamai8_",
     linkedin: "https://www.linkedin.com/in/osamaislam",
     github: "http://github.com/osamai8",
-    resume: "",
+    resume: "https://drive.google.com/file/d/1LJsnjvhH9fOJM9IgC-IVEAQhVBC6MYzn/view?usp=sharing",
     templateOwner: "https://thaer.shop"
 }
 export const userEmail = "islamosama488@gmail.com";
